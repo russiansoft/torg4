53a22952039def34f941760a19cd42ee5820adcb
russiansoft <russiansoft@mail.ru>
2022-06-02 07:15:26 +0300

    Страница товара

--- public/товар.js
+++ public/товар.js
@@ -9,6 +9,35 @@ addEventListener("load", async function()
 		let id = url.searchParams.get("id");
 		document.record = await dataset.find(id);
 		document.title = document.record.title;
-		DataOut(document.record);
+		//DataOut(document.record);
+
+		document.record.Описание = document.record.Описание.replace(/\n/g, "<br/>");
+
+		let template = new Template("#content").fill(document.record);
+
+		let file = document.record.Изображение;
+		if (file)
+		{
+			let attributes = { };
+			for (let part of file.split("|"))
+			{
+				if (!part)
+					continue;
+				let pair = part.split(":");
+				attributes[pair[0]] = pair[1];
+			}
+			attributes.address = attributes.address.replace(/\\/g, "/");
+			let base64 = await hive.get(attributes.address);
+			let image = "data:image/jpeg;base64," + base64.content;
+			template.fill( { "image": image } );
+		}
+	
+		let qr = "https://xn--40-6kcai3c0bf.xn--p1ai/?id=" + document.record.id;
+		template.fill( { "qr": qr } );
+
+		template.out("main");
+
+		let qrcode = new QRCode(element("main #qrcode"));
+		qrcode.makeCode(qr);
 	}
 } );
